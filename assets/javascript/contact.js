f2efcd8a436b7adc134b074e829fce334f44490d
maryam-jafari <jafarim1029154@gmail.com>
2023-10-24 13:39:59 +0330

    finished contact.

---
+++ assets/javascript/contact.js
@@ -0,0 +1,19 @@
+var swiper = new Swiper(".contact", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+
+    pagination: {
+
+        el: '.swiper-pagination',
+        clickable: true,
+
+
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+});
